7218b786e766a51ed71968fdd32c9d0308cf2e6e
Caruy Chen <cchen@student.hive.fi>
2022-04-16 16:41:03 +0300

    fix navwidth

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="css/home.css">
-		<link rel="stylesheet" href="css/cards.css">
+		<link rel="stylesheet" href="./css/home.css">
+		<link rel="stylesheet" href="./css/cards.css">
 		<title>Super666Shop</title>
 	</head>
 	<body>
