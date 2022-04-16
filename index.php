39ddd33d96e654308924a46174ef73ecc3892d2e
Caruy Chen <cchen@student.hive.fi>
2022-04-16 10:57:49 +0300

    basic nav

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/home.css">
+		<title>Super666Shop</title>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul class="horizontal gray">
+					<li><a href="#home">Home</a></li>
+					<li><a href="#login">Login</a></li>
+					<li class="rightli" style="float:right"><a href="#basket">Basket</a></li>
+				</ul>
+			</nav>
+		</header>	
+		<footer>
+			<hr />
+			<p>&copy parkharo & cchen 2022</p>
+		</footer>
+	</body>
+</html>
