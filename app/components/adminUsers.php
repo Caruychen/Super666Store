db46ffcc328c9d285adfcb73fa6494f211718dff
Caruy Chen <cchen@student.hive.fi>
2022-04-17 20:35:20 +0300

    add remove user feature

--- app/components/adminUsers.php
+++ app/components/adminUsers.php
@@ -7,12 +7,20 @@
         <table class="admin-table">
             <tr>
                 <th>User</th>
+                <th>Manage</th>
             </tr>
             <?php
                 foreach ($users as $user)
                 {
                     echo "<tr>";
                     echo "<td>$user[login]</td>";
+                    if ($user['login'] !== 'admin')
+                        echo "<td>
+                        <form action='index.php?page=admin&admin=users' method='POST'>
+                            <input type='hidden' name='logintoremove' value='$user[login]' />
+                            <input type='submit' name='adminuser' value='Remove' />
+                        </form>
+                        </td>";
                     echo "</tr>";
                 }
             ?>
--- store/dispatcher.php
+++ store/dispatcher.php
@@ -69,4 +69,8 @@
 			logout();
 		} 
 	}
+	if (isset($_POST['adminuser']) && $_POST['adminuser'] == "Remove" && is_admin())
+	{
+		del_user($_POST['logintoremove']);
+	}
 ?>
