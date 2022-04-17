f6e547415e7046ac250c7f78dbe82b0543aaf836
Caruy Chen <cchen@student.hive.fi>
2022-04-17 17:24:36 +0300

    move validator to orders.php

--- install.php
+++ install.php
@@ -1,4 +1,5 @@
 <?php 
+    include "./store/orders.php";
 	include "./store/user_manager.php";
     include "./store/sessions_manager.php";
     include "./store/dispatcher.php";
@@ -22,21 +23,6 @@
                 );
             }
             fclose($handle);
-        }
-    }
-    function validate_basket()
-    {
-        date_default_timezone_set('Europe/Helsinki');
-        $date = "[".date("D M j G:i:s T Y")."]";
-        $user = $_SESSION['loggued_on_user'];
-        if ($handle = fopen('./data/orders/orders.csv', 'a'))
-        {
-            flock($handle, LOCK_EX);
-            foreach ($_SESSION['basket'] as $key => $array)
-                fputcsv($handle, [$date, $user, $key, $array['quantity']]);
-            flock($handle, LOCK_UN);
-            fclose($handle);
-            clear_basket();
         }
     }
 ?>
--- store/orders.php
+++ store/orders.php
@@ -1,3 +1,17 @@
 <?PHP
-	
+   function validate_basket()
+    {
+        date_default_timezone_set('Europe/Helsinki');
+        $date = "[".date("D M j G:i:s T Y")."]";
+        $user = $_SESSION['loggued_on_user'];
+        if ($handle = fopen('./data/orders/orders.csv', 'a'))
+        {
+            flock($handle, LOCK_EX);
+            foreach ($_SESSION['basket'] as $key => $array)
+                fputcsv($handle, [$date, $user, $key, $array['quantity']]);
+            flock($handle, LOCK_UN);
+            fclose($handle);
+            clear_basket();
+        }
+    }
 ?>
