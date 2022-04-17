bd8a082855f4709c73ad2e50f26a03dc94ad3468
Caruy Chen <cchen@student.hive.fi>
2022-04-17 19:05:22 +0300

    fix formatting

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- FONTS -->  
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Palanquin:wght@100;400;700&family=Roboto:wght@100;700&display=swap" rel="stylesheet">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Palanquin:wght@100;400;700&family=Roboto:wght@100;700&display=swap" rel="stylesheet">
 	<!-- CSS  --> 		
-<link rel="stylesheet" href="./css/home.css">
+		<link rel="stylesheet" href="./css/home.css">
 		<link rel="stylesheet" href="./css/cards.css">
 		<title>Superpower 666SuperShop</title>
 	</head>
