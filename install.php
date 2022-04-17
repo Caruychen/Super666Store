e8eb46581e2bfb87307c68c8de3b772eac165f61
Caruy Chen <cchen@student.hive.fi>
2022-04-17 14:06:30 +0300

    added flock

--- install.php
+++ install.php
@@ -28,8 +28,10 @@
         $user = $_SESSION['loggued_on_user'];
         if ($handle = fopen('./data/orders/orders.csv', 'a'))
         {
+            flock($handle, LOCK_EX);
             foreach ($_SESSION['basket'] as $key => $array)
                 fputcsv($handle, [$date, $user, $key, $array['quantity']]);
+            flock($handle, LOCK_UN);
             fclose($handle);
             clear_basket();
         }
