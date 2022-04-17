482fadd882ab429b45c1aead06b8e7f3ca9bbf00
Pavel Arkharov <parkharo@c1r1p11.hive.fi>
2022-04-17 15:13:42 +0300

    styled a bit

--- app/views/account.php
+++ app/views/account.php
@@ -1,12 +1,14 @@
 <?php
+
+
 	if (!isset($_SESSION['loggued_on_user']) ||empty($_SESSION['loggued_on_user']))
 		header('Location: index.php?page=login');
 	$login = $_SESSION['loggued_on_user'];
 	$basket = $_SESSION['basket'];
 	?>
 
-	<div>
-		<h2>Manage your account</h2>
+	<div class="gray leftnav">
+		<h4>Manage your account</h4>
 
 		<ul>
 			<li><a href="?page=delete">Delete account</a></li>
